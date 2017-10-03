55c6ccd021546812dbcaaaf5d1ebcbf2e9ed1f92
Wu Hsiu Rong <mandy19970219@gm.student.ncut.edu.tw>
2017-10-03 17:31:05 +0800

    deletemission

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,11 @@ use Illuminate\Http\Request;
  * 顯示所有任務
  */
 Route::get('/', function () {
-    return view('tasks');
+    $tasks = Task::orderBy('created_at', 'asc')->get();
+
+    return view('tasks', [
+        'tasks' => $tasks
+    ]);
 
 });
 
@@ -18,13 +22,39 @@ Route::get('/', function () {
  * 增加新的任務
  */
 Route::post('/task', function (Request $request) {
-    //
+    $validator = Validator::make($request->all(), [
+        'name' => 'required|max:255',
+    ]);
+
+    if ($validator->fails()) {
+        return redirect('/')
+            ->withInput()
+            ->withErrors($validator);
+    }
+
+    $validator = Validator::make($request->all(), [
+        'name' => 'required|max:255',
+    ]);
+
+    if ($validator->fails()) {
+        return redirect('/')
+            ->withInput()
+            ->withErrors($validator);
+    }
+
+    $task = new Task;
+    $task->name = $request->name;
+    $task->save();
+
+    return redirect('/');
 });
 
 /**
  * 刪除任務
  */
 Route::delete('/task/{task}', function (Task $task) {
-    //
+    $task->delete();
+
+    return redirect('/');
 });
 //});
